a4764ab6078d1d1f61da7662712c4da219edd0d1
rileyaitken <rai29@uclive.ac.nz>
2018-05-20 14:04:22 +1200

    yeet

--- cplexassignment.py
+++ cplexassignment.py
@@ -1,7 +1,6 @@
 import cplex
 import sys
 
-my_obj = []
 my_rhs = []
 var_names = []
 constraint_names = []
@@ -21,28 +20,45 @@ def main(argv):
 	except IndexError as e:
 		print(e)
 		
+	c = cplex.Cplex()
+		
 	for i in range(1, X + 1):
 		for j in range(1, Y + 1):
 			for k in range(1, Z + 1):
 				variable = 'x' + str(i) + str(j) + str(k)
-				var_names.append(variable)
-				my_obj.append(1.0)
+				c.variables.add(names = [variable], obj = [1.0], lb = [0.0], ub = [cplex.infinity])
 	
 	#demand volume constraints	
 	for i in range(0, len(var_names), Y * Z):
 		for j in range(0, Z):
-			constraint_name = 'h' + var_names[i][1] + var_names[j][3]
-			constraint_rhs = []
-			constraint_lhs = []
-			for k in range(0, Y): #Get all the paths/variables between i and j
-				constraint_lhs.append(j + i + (Z * k)) #Give index of variable in var_names
-				constraint_rhs.append(1.0) #Coefficients, always 1.0 in this case
-			constraint = [constraint_lhs, constraint_rhs]
-			my_senses.append('E') #Equality constraint between sum of all path variables between source i and dest j, and i + j
-			my_rhs.append(int(var_names[i][1]) + int(var_names[j][3])) #Set demand volume as i + j
-			print(my_senses[-1], my_rhs[-1],constraint, constraint_name)
+			constraint_name = ['h' + var_names[i][1] + var_names[j][3]]
+			constraint_varnames = []
+			constraint_coefficients = []
+			for k in range(1, Y + 1): #Get all the paths/variables between i and j
+				constraint_varnames.append('x' + var_names[i][1] + str(k) + var_names[j][3]) #Can give name of variable, Cplex will find it in var_names
+				constraint_coefficients.append(1.0) #Coefficients, always 1.0 in this case
+			constraint = [constraint_varnames, constraint_coefficients]
+			my_sense = ['E'] #Equality constraint between sum of all path variables between source i and dest j, and i + j
+			my_rhs = [int(var_names[i][1]) + int(var_names[j][3])] #Set demand volume as i + j
+			print(my_sense, my_rhs, constraint, constraint_name)
+			c.linear_constraints.add(lin_expr = constraint, senses = my_sense, rhs = my_rhs, names = constraint_name)
 		
-	
+	for i in range(1, X + 1):
+		for j in range(1, Z + 1):
+			constraint_varnames = []
+			constraint_coefficients = []
+			constraint_name = 'pathUtil' + str(i) + str(j)
+			print(constraint_name)
+			for k in range(1, Y + 1):
+				variable = 'U' + str(i) + str(k) + str(j)
+				c.variables.add(names = [variable], obj = [1.0], lb = [0.0], ub = [cplex.infinity], types = [c.variables.type.binary])
+				constraint_varnames.append(variable)
+				constraint_coefficients.append(1.0)
+			constraint = [constraint_varnames, constraint_coefficients]
+			my_sense = ['E']
+			my_rhs = [3.0]
+			print(my_sense, my_rhs, constraint, constraint_name)
+			c.linear_constraints.add(lin_expr = constraint, senses = my_sense, rhs = my_rhs, names = constraint_name)
 		
 	
 def populatebyrows(prob):
