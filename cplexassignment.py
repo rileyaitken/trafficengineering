e7c46f7e54d41173ce4b926a3d0c1b2f6a54ae36
rileyaitken <rai29@uclive.ac.nz>
2018-05-31 13:56:55 +1200

    fuck yea boiii

--- cplexassignment.py
+++ cplexassignment.py
@@ -26,30 +26,32 @@ def main(argv):
 	for i in range(1, X + 1):
 		for j in range(1, Z + 1):
 			sum_volumes += i + j
-		
+	
+	sum_inversed = 1 / sum_volumes	
 	print(sum_volumes)	
 		
 	c = cplex.Cplex()
+	c.objective.set_sense(c.objective.sense.minimize)
 		
 	for i in range(1, X + 1):
 		for j in range(1, Y + 1):
 			for k in range(1, Z + 1):
 				variable = 'x' + str(i) + str(j) + str(k)
-				c.variables.add(names = [variable], obj = [1.0], lb = [0.0], ub = [cplex.infinity])
+				c.variables.add(names = [variable], lb = [0.0], ub = [cplex.infinity])
 	
 	#demand volume constraints	
-	for i in range(0, len(var_names), Y * Z):
-		for j in range(0, Z):
-			constraint_name = ['h' + var_names[i][1] + var_names[j][3]]
+	for i in range(1, X + 1):
+		for j in range(1, Z + 1):
+			constraint_name = ['h' + str(i) + str(j)]
 			constraint_varnames = []
 			constraint_coefficients = []
 			for k in range(1, Y + 1): #Get all the paths/variables between i and j
-				constraint_varnames.append('x' + var_names[i][1] + str(k) + var_names[j][3]) #Can give name of variable, Cplex will find it in var_names
+				constraint_varnames.append('x' + str(i) + str(k) + str(j)) #Can give name of variable, Cplex will find it in var_names
 				constraint_coefficients.append(1.0) #Coefficients, always 1.0 in this case
 			constraint = [constraint_varnames, constraint_coefficients]
-			my_sense = ['E'] #Equality constraint between sum of all path variables between source i and dest j, and i + j
-			my_rhs = [int(var_names[i][1]) + int(var_names[j][3])] #Set demand volume as i + j
-			c.linear_constraints.add(lin_expr = constraint, senses = my_sense, rhs = my_rhs, names = constraint_name)
+			my_sense = "E" #Equality constraint between sum of all path variables between source i and dest j, and i + j
+			my_rhs = [i + j] #Set demand volume as i + j
+			c.linear_constraints.add(lin_expr = [constraint], senses = my_sense, rhs = my_rhs, names = constraint_name)
 		
 	for i in range(1, X + 1):
 		for j in range(1, Z + 1):
@@ -59,7 +61,7 @@ def main(argv):
 			print(constraint_name)
 			for k in range(1, Y + 1):
 				variable = 'U' + str(i) + str(j) + str(i) + str(k) + str(j)
-				c.variables.add(names = [variable], obj = [1.0], lb = [0.0], ub = [cplex.infinity], types = [c.variables.type.binary])
+				c.variables.add(names = [variable], lb = [0.0], ub = [1.0], types = [c.variables.type.binary])
 				constraint_varnames.append(variable)
 				constraint_coefficients.append(1.0)
 			constraint = [constraint_varnames, constraint_coefficients]
@@ -114,7 +116,7 @@ def main(argv):
 			constraint_name = ['capacC' + str(i) + str(k)]
 			print(constraint_name)
 			variable = 'c' + str(i) + str(k)
-			c.variables.add(names = [variable], obj = [1.0], lb = [0.0], ub = [cplex.infinity])
+			c.variables.add(names = [variable], lb = [0.0], ub = [cplex.infinity])
 			constraint_varnames = []
 			constraint_coefficients = []
 			for j in range(1, Z + 1):
@@ -130,10 +132,10 @@ def main(argv):
 			
 	for k in range(1, Y + 1):
 		for j in range(1, Z + 1):
-			constraint_name = ['capacC' + str(k) + str(j)]
+			constraint_name = ['capacD' + str(k) + str(j)]
 			print(constraint_name)
 			variable = 'd' + str(k) + str(j)
-			c.variables.add(names = [variable], obj = [1.0], lb = [0.0], ub = [cplex.infinity])
+			c.variables.add(names = [variable], lb = [0.0], ub = [cplex.infinity])
 			constraint_varnames = []
 			constraint_coefficients = []
 			for i in range(1, X + 1):
@@ -145,17 +147,44 @@ def main(argv):
 			my_sense = "E"
 			my_rhs = [0.0]
 			print(my_sense, my_rhs, constraint, constraint_name)
-			c.linear_constraints.add(lin_expr = [constraint], senses = my_sense, rhs = my_rhs, names = constraint_name)	
+			c.linear_constraints.add(lin_expr = [constraint], senses = my_sense, rhs = my_rhs, names = constraint_name)
+			
+	for k in range(1, Y + 1):
+		constraint_name = ['load' + str(k)]
+		variable = 'loadk' + str(k)
+		c.variables.add(names = [variable], lb = [0.0], ub = [cplex.infinity])
+		constraint_varnames = []
+		constraint_coefficients = []
+		for i in range(1, X + 1):
+			for j in range(1, Z + 1):
+				constraint_varnames.append('x' + str(i) + str(k) + str(j))
+				constraint_coefficients.append(1.0)
+		constraint_varnames.append(variable)
+		constraint_coefficients.append(-1.0)
+		constraint = [constraint_varnames, constraint_coefficients]
+		my_sense = "E"
+		my_rhs = [0.0]
+		print(my_sense, my_rhs, constraint, constraint_name)
+		c.linear_constraints.add(lin_expr = [constraint], senses = my_sense, rhs = my_rhs, names = constraint_name)		
 			
 			
+	c.variables.add(names = ["r"], obj = [1.0], lb = [0.0], ub = [1.0])
+	
+	for k in range(1, Y + 1):
+		constraint_name = ['loadbalance' + str(k)]
+		constraint_varnames = ["r", 'loadk' + str(k)]
+		constraint_coefficients = [1.0, -sum_inversed]
+		constraint = [constraint_varnames, constraint_coefficients]
+		my_sense = "G"
+		my_rhs = [0.0]
+		print(my_sense, my_rhs, constraint, constraint_name)
+		c.linear_constraints.add(lin_expr = [constraint], senses = my_sense, rhs = my_rhs, names = constraint_name)		
+	
+		
+	c.write("instance" + str(X) + str(Y) + str(Z) + ".lp")
+	c.solve()
 			
-			
-
-				
-
-
-    
-
+		
 def populatebyrows(prob):
 	prob.objective.set_sense(prob.objective.sense.minimize)
 	
